a2a9b16f9cbb3b43484c8d9675dc667593489e74
nomayen <nomayen.ohin@gmail.com>
2023-04-08 13:37:50 +0600

    server create

---
+++ routes/hotels.js
@@ -0,0 +1,60 @@
+import  express  from "express";
+import Hotel from "../models/Hotel.js";
+import { get } from "mongoose";
+
+const router =express.Router();
+// create 
+router.post("/",async(req, res)=>{
+
+    const newHotel = new Hotel(req.body)
+    try {
+        const saveHotels =await newHotel.save()
+        res.status(200).json(saveHotels)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+// update 
+router.put("/:id",async(req, res)=>{
+    try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body},{new:true})
+        res.status(200).json(updatedHotel)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+// Delete 
+router.delete("/:id",async(req, res)=>{
+    try {
+        await Hotel.findByIdAndDelete(req.params.id);
+        res.status(200).json("Hotel has Been deleted")
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+// get
+
+router.get("/:id",async(req, res)=>{
+    try {
+        const hotel = await Hotel.findById(req.params.id);
+        res.status(200).json(hotel)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+// get all 
+
+
+router.get("/",async(req, res)=>{
+    try {
+        const hotels = await Hotel.find(req.params.id);
+        res.status(200).json(hotels)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+});
+
+ 
+export default router
